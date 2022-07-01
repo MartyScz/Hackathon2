e2c1b39b3203e5ad037e8ad989aa8d4ccec3fad1
MariePerraud <m100chez@gmail.com>
2022-07-01 05:19:02 +0200

    modifs page accueil suite echec merge

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -1,33 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Connexion.css";
-import logo from "../assets/logo-apside-blanc.svg";
+import logo from "../assets/logo-apside-blanc.png";
 import Motion from "../components/Motion";
 
 function Connexion() {
   return (
     <div className="connexion-container">
-      <h2>INTO</h2>
+      <Motion />
       <div className="div-logo-connexion">
         <img className="logo-connexion" src={logo} alt="logo apside bleu" />
-
-        <Link
-          to="/Login"
-          className="log-button"
-          style={{ color: "white", marginLeft: "510px", marginTop: "-600px" }}
-        >
+      </div>
+      <div className="div-link1">
+        <Link to="/Login" className="log-button" style={{ color: "white" }}>
           CONNEXION
         </Link>
+      </div>
+      <div className="div-link2">
         <Link
           to="/Register"
           className="register-button"
-          style={{ color: "white", marginLeft: "903px", marginTop: "180px" }}
+          style={{ color: "white" }}
         >
           REGISTER
         </Link>
       </div>
-
-      <Motion />
     </div>
   );
 }
